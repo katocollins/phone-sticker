eb0955a9cd02feadbb8ce83e80b2531bc5bdae36
katocollins <ckato131@gmail.com>
2025-04-02 23:29:52 +0300

    feat: Project Initialization

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-blue-800 justify-center h-40 justify-items-center w-40">
+        <h1>Phone-Sticker-App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
